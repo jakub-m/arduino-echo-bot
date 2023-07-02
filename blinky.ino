7a14c2a76355ef26f5d0c3219e976cc41a3c30c1
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2023-07-02 18:06:41 +0200

    split ino

--- blinky.ino
+++ blinky.ino
@@ -1,68 +1,136 @@
+#include "src/interval.h"
+
 typedef uint8_t pin;
 
-/**
- * Interval. The base unit is microsecond, and this can be converted to other units with intervalMillis or intervalMicro.
- */
-typedef long interval;
+const pin trig_pin = 9;
+const pin echo_pin = 10;
+const pin led_pin = 13;
 
-/**
- * Return interval as milliseconds.
- */
-inline long interval_millis(interval dt)
+void toggle_led()
 {
-    return dt / 1000;
+    if (digitalRead(led_pin) == HIGH)
+    {
+        digitalWrite(led_pin, LOW);
+    }
+    else
+    {
+        digitalWrite(led_pin, HIGH);
+    }
 }
 
-/**
- * Return interval as microseconds.
- */
-inline long interval_micro(interval dt)
+// A structure to manage asynchronous measurement.
+struct distance_measurement
 {
-    return dt;
+    // Start of the measurement, elapsed microseconds.
+    unsigned long start_micros;
+    // Last measurement in cm.
+    float distance_cm;
+    // Is there a measurement in flight?
+    bool in_flight;
+};
+
+struct debug_info
+{
+    unsigned long trigger_count;
+    unsigned long echo_timeout;
+};
+
+struct distance_measurement dm;
+struct debug_info di;
+unsigned long next_report = 0;
+
+// Measure distance without blocking.
+void measure_distance_async(struct distance_measurement *dm)
+{
+    unsigned long now_micros = micros();
+    if (dm->in_flight)
+    {
+        unsigned long pulse = pulseIn(echo_pin, HIGH); // remove this, do it on my own
+        unsigned long dt_micros = now_micros - dm->start_micros;
+        Serial.print("pulse ");
+        Serial.println(pulse);
+        Serial.print("dt_micros ");
+        Serial.println(dt_micros);
+
+        if (digitalRead(echo_pin) == HIGH)
+        {
+            dm->in_flight = false;
+            // Speed of sound is .0343 c/uS, which is 34.3 cm per mS
+            dm->distance_cm = 0.0343 * dt_micros / 2;
+        }
+        else
+        {
+            int max_wait_time = 10 * INTERVAL_MS;
+            // Break measurement when the puls never came back.
+            if (dt_micros > interval_micros(max_wait_time))
+            {
+                di.echo_timeout++;
+                dm->in_flight = false;
+            }
+        }
+    }
+    else
+    {
+        // No measurement in flight, trigger one.
+        dm->start_micros = micros();
+        dm->in_flight = true;
+        trigger_echo();
+    }
 }
 
-const interval us = 1;
-const interval ms = 1000 * us;
-const interval sec = 1000 * ms;
+// float measure_distance_cm()
+//{
+//     trigger_echo();
+//
+//     unsigned long durationUs = pulseIn(echo_pin, HIGH, interval_micros(10 * ms));
+//     // Speed of sound is .0343 c/uS, which is 34.3 cm per ms
+//     const float vs_cm_us = 0.0343;
+//     float distance = vs_cm_us * durationUs / 2;
+//     return distance;
+// }
 
-const pin trigPin = 9;
-const pin echoPin = 10;
-const pin ledPin = 13;
+void trigger_echo()
+{
+    di.trigger_count++;
+    digitalWrite(trig_pin, LOW);
+    delayMicroseconds(interval_micros(2 * INTERVAL_US));
+    digitalWrite(trig_pin, HIGH);
+    delayMicroseconds(interval_micros(10 * INTERVAL_US));
+    digitalWrite(trig_pin, LOW);
+}
 
 void setup()
 {
-    pinMode(ledPin, OUTPUT);
-    pinMode(trigPin, OUTPUT);
-    pinMode(echoPin, INPUT);
+    pinMode(led_pin, OUTPUT);
+    pinMode(trig_pin, OUTPUT);
+    pinMode(echo_pin, INPUT);
     Serial.begin(9600);
+    dm.distance_cm = 0;
+    dm.in_flight = false;
+    di.trigger_count = 0;
+    di.echo_timeout = 0;
 }
 
 void loop()
 {
-    float distanceCm = measure_distance_cm();
-    Serial.println(distanceCm);
-    delay(interval_millis(300 * ms));
-    if (digitalRead(ledPin) == HIGH)
+    unsigned long now = millis();
+    measure_distance_async(&dm);
+    if (now > next_report)
     {
-        digitalWrite(ledPin, LOW);
+        Serial.print("distance cm ");
+        Serial.println(dm.distance_cm);
+        Serial.print("trigger count ");
+        Serial.println(di.trigger_count);
+        Serial.print("timeout ");
+        Serial.println(di.echo_timeout);
+        Serial.print("start time ");
+        Serial.println(dm.start_micros);
+        // Serial.println(300);
+        next_report = now + interval_millis(1 * INTERVAL_SEC);
+        toggle_led();
     }
-    else
-    {
-        digitalWrite(ledPin, HIGH);
-    }
-}
 
-float measure_distance_cm()
-{
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(interval_micro(2 * us));
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(interval_micro(10 * us));
-    digitalWrite(trigPin, LOW);
-
-    unsigned long durationUs = pulseIn(echoPin, HIGH, interval_micro(10 * ms));
-    // Speed of sound is .0343 c/uS, which is 34.3 cm per ms
-    const float vs_cm_us = 0.0343;
-    float distance = vs_cm_us * durationUs / 2;
-    return distance;
+    //    float distanceCm = measure_distance_cm();
+    //    Serial.println(distanceCm);
+    //    delay(interval_millis(300 * ms));
 }
---
+++ src/interval.h
@@ -0,0 +1,35 @@
+#ifndef __INTERVAL_H__
+#define __INTERVAL_H__
+
+/**
+ * Interval. The base unit is microsecond, and this can be converted to other units with intervalMillis or intervalMicro.
+ */
+typedef long interval;
+
+#define INTERVAL_US (1)
+#define INTERVAL_MS (1000 * INTERVAL_US)
+#define INTERVAL_SEC (1000 * INTERVAL_MS)
+
+/**
+ * Return interval as milliseconds.
+ */
+long interval_millis(interval dt);
+
+/**
+ * Return interval as microseconds.
+ */
+long interval_micros(interval dt);
+
+#include "interval.c"
+
+inline long interval_millis(interval dt)
+{
+    return dt / INTERVAL_MS;
+}
+
+inline long interval_micros(interval dt)
+{
+    return dt / INTERVAL_US;
+}
+
+#endif // __INTERVAL_H__
